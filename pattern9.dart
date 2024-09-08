c3ae16316d68a0ff6a0c38605ac7ac57cb8f66b6
Sharayu Bhad <140377743+sharayubhad@users.noreply.github.com>
2024-09-08 23:37:24 +0530

    Add files via upload

---
+++ pattern9.dart
@@ -0,0 +1,17 @@
+import "dart:io";               ///ERROR CODE
+
+void main(){
+  print("Enter the number of rows:");
+  int? rows=int.parse(stdin.readLineSync()!);
+
+  int number=1;
+  for(int i=0;i<rows;i++){
+
+    for(int j=0;j<rows;j++){
+      stdout.write("$number ");
+      number++;
+      }
+    }
+    print(" ");
+    number--;
+}
